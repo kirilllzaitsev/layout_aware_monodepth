066eec2cbd12bd1e92aed5a25070ef4c0319f4ce
kirilllzaitsev <kzaitsev88@gmail.com>
2023-07-16 16:29:19 +0200

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="monodepth_project",
+    version="0.0.1",
+    description="",
+    author="Kirill Zaitsev",
+    author_email="kzaitsev88@gmail.com",
+    packages=find_packages(),
+    zip_safe=False,
+)
