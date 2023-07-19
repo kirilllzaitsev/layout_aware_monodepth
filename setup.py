a6f075692af93c83ef06249a62b9c9dea9432c75
kirilllzaitsev <kzaitsev88@gmail.com>
2023-07-19 17:50:02 +0200

    add setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="monodepth_project",
+    name="layout_aware_monodepth",
     version="0.0.1",
     description="",
     author="Kirill Zaitsev",
