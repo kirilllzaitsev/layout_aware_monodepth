46c1e3efcb91da8dd949d5c5743b61e8f34ff001
kirilllzaitsev <kzaitsev88@gmail.com>
2023-07-24 17:52:02 +0200

    adjust plots

--- layout_aware_monodepth/vis_utils.py
+++ layout_aware_monodepth/vis_utils.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 def plot_samples_and_preds(batch: dict, preds):
     batch_size = len(batch["image"])
-    fig, axs = plt.subplots(batch_size, 3, figsize=(batch_size * 5, 3))
+    fig, axs = plt.subplots(batch_size, 3, figsize=(max(batch_size*5, 10), batch_size*5))
     for i in range(batch_size):
         img = batch["image"][i].permute(1, 2, 0)
         in_depth = batch["depth"][i]
@@ -31,6 +31,10 @@ def plot_samples_and_preds(batch: dict, preds):
         for ax in [ax_0, ax_1, ax_2]:
             ax.axis("off")
             ax.set_aspect("equal")
+        if i == 0:
+            ax_0.set_title("Image", fontsize=20)
+            ax_1.set_title("Input Depth", fontsize=20)
+            ax_2.set_title("Predicted Depth", fontsize=20)
     plt.tight_layout()
     fig.subplots_adjust(wspace=0, hspace=0.0)
     return fig
