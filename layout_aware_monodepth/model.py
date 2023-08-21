cc5650877e563d9bd7e1c9c9fdb02c63ab914dd5
kirilllzaitsev <kzaitsev88@gmail.com>
2023-08-21 22:40:16 +0200

    rm dummy_input from model

--- layout_aware_monodepth/model.py
+++ layout_aware_monodepth/model.py
@@ -46,15 +46,8 @@ class DepthModel(nn.Module):
             assert (
                 encoder_channel_spec is not None
             ), f"Must provide encoder channel spec for {encoder_name=}"
-            dummy_input = torch.randn(1, self.encoder.out_channels[1], 128, 128)
             for block_idx in range(len(encoder_channel_spec)):
                 x_dim = encoder_channel_spec[block_idx]
-                # if encoder_channel_spec_exists:
-                #     x_dim = encoder_channel_spec[block_idx]
-                # else:
-                #     dummy_input = self.encoder.model.blocks[block_idx](dummy_input)
-                #     x_dim = dummy_input.shape[1]
-                #     print(f"{x_dim=}")
                 if use_attn:
                     block = AttentionBasicBlockB(
                         x_dim,
