8175563d419ed09fbeb8b6c991ec0131526ca6a8
kirilllzaitsev <kzaitsev88@gmail.com>
2023-08-28 22:05:22 +0200

    possibility to use local attention before SE block

--- layout_aware_monodepth/model.py
+++ layout_aware_monodepth/model.py
@@ -17,7 +17,9 @@ class DepthModel(nn.Module):
         encoder_name="timm-mobilenetv3_large_100",
         encoder_weights="imagenet",
         decoder_activation="sigmoid",
+        decoder_attention_type=None,
         decoder_first_channel=256,
+        do_insert_after=True,
         in_channels=3,
         use_attn=False,
         use_extra_conv=False,
@@ -36,6 +38,7 @@ class DepthModel(nn.Module):
             activation=decoder_activation,
             encoder_depth=len(decoder_channels),
             decoder_channels=decoder_channels,
+            decoder_attention_type=decoder_attention_type,
         )
 
         self.encoder = model.encoder
@@ -47,33 +50,75 @@ class DepthModel(nn.Module):
             assert (
                 encoder_channel_spec is not None
             ), f"Must provide encoder channel spec for {encoder_name=}"
-            for block_idx in range(len(encoder_channel_spec)):
-                x_dim = encoder_channel_spec[block_idx]
-                if use_attn:
-                    block = AttentionBasicBlockB(
-                        x_dim,
-                        x_dim,
-                        stride=1,
-                        heads=4,
-                        window_size=4,
-                    )
-                elif use_extra_conv:
-                    block = nn.Sequential(
-                        nn.Conv2d(x_dim, x_dim, kernel_size=1, padding=0),
-                        nn.ReLU(inplace=True),
-                        nn.Conv2d(x_dim, x_dim, kernel_size=1, padding=0),
-                        nn.ReLU(inplace=True),
-                    )
-                else:
-                    block = nn.Identity()
-                if self.encoder_name == "timm-mobilenetv3_large_100":
-                    self.encoder.model.blocks[block_idx] = nn.Sequential(
-                        self.encoder.model.blocks[block_idx], block
-                    )
-                elif self.encoder_name == "resnet18":
-                    layer = getattr(self.encoder, f"layer{block_idx+1}")
-                    layer = nn.Sequential(layer, block)
-                    setattr(self.encoder, f"layer{block_idx+1}", layer)
+
+            if do_insert_after:
+                for block_idx in range(len(encoder_channel_spec)):
+                    x_dim = encoder_channel_spec[block_idx]
+                    if use_attn:
+                        block = AttentionBasicBlockB(
+                            x_dim,
+                            x_dim,
+                            stride=1,
+                            heads=4,
+                            window_size=4,
+                        )
+                    elif use_extra_conv:
+                        block = nn.Sequential(
+                            nn.Conv2d(x_dim, x_dim, kernel_size=1, padding=0),
+                            nn.ReLU(inplace=True),
+                            nn.Conv2d(x_dim, x_dim, kernel_size=1, padding=0),
+                            nn.ReLU(inplace=True),
+                        )
+                    else:
+                        block = nn.Identity()
+                    if self.encoder_name == "timm-mobilenetv3_large_100":
+                        self.encoder.model.blocks[block_idx] = nn.Sequential(
+                            self.encoder.model.blocks[block_idx], block
+                        )
+                    elif self.encoder_name == "resnet18":
+                        layer = getattr(self.encoder, f"layer{block_idx+1}")
+                        layer = nn.Sequential(layer, block)
+                        setattr(self.encoder, f"layer{block_idx+1}", layer)
+
+            elif self.encoder_name == "timm-mobilenetv3_large_100":
+                for block_idx in range(len(self.encoder.model.blocks) - 1):
+                    for subblock_idx in range(
+                        len(self.encoder.model.blocks[block_idx])
+                    ):
+                        if not isinstance(
+                            self.encoder.model.blocks[block_idx][subblock_idx].se,
+                            nn.Identity,
+                        ):
+                            x_dim = model.encoder.model.blocks[block_idx][subblock_idx].se.conv_reduce.in_channels  # fmt: skip
+                        else:
+                            x_dim = model.encoder.model.blocks[block_idx][subblock_idx].bn2.bias.numel()  # fmt: skip
+
+                        if use_attn:
+                            block = AttentionBasicBlockB(
+                                x_dim,
+                                x_dim,
+                                stride=1,
+                                heads=4,
+                                window_size=4,
+                            )
+                        elif use_extra_conv:
+                            block = nn.Sequential(
+                                nn.Conv2d(x_dim, x_dim, kernel_size=1, padding=0),
+                                nn.ReLU(inplace=True),
+                                nn.Conv2d(x_dim, x_dim, kernel_size=1, padding=0),
+                                nn.ReLU(inplace=True),
+                            )
+                        else:
+                            block = nn.Identity()
+                        if self.encoder_name == "timm-mobilenetv3_large_100":
+                            base_block = model.encoder.model.blocks[block_idx][
+                                subblock_idx
+                            ]
+                            new_se_block = nn.Sequential(
+                                block,
+                                base_block.se,
+                            )
+                            base_block.se = new_se_block
 
         self.decoder = model.decoder
         self.depth_head = model.segmentation_head
