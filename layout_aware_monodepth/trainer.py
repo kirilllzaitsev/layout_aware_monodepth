61b1ed2b8cdabc7dec5743f162b214398cb020a6
kirilllzaitsev <kzaitsev88@gmail.com>
2023-10-15 08:44:33 +0200

    tweak training to use confidence in preds

--- layout_aware_monodepth/trainer.py
+++ layout_aware_monodepth/trainer.py
@@ -15,12 +15,16 @@ class Trainer:
         optimizer,
         criterion,
         device,
+        clf_model=None,
+        postproc_block=None,
     ):
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
         self.args = args
         self.device = device
+        self.clf_model = clf_model
+        self.postproc_block = postproc_block
 
     def train_step(self, model, batch, criterion, optimizer):
         model.train()
@@ -39,7 +43,36 @@ class Trainer:
             out = model(x, batch["line_embed"].to(self.device))
         else:
             out = model(x)
+        if self.clf_model is not None:
+            assert self.postproc_block is not None
+
+            line_res = model.decoder.get_deeplsd_pred(x)
+
+            df_embed = line_res["df_embed"]
+            concat1 = torch.cat([out, df_embed], dim=1)
+            init_shape = concat1.shape[-2:]
+            import torchvision.transforms.functional as fn
+
+            concat = self.resize_tensor(concat1, [i // 2 for i in init_shape])
+            postproc_out = self.postproc_block(concat)
+            postproc_out = self.resize_tensor(postproc_out, init_shape)
+
+            pred_concat = torch.cat((out, postproc_out), dim=1)
+            df = line_res["df_norm"].unsqueeze(1)
+            clf_inputs = torch.cat((pred_concat, x, df), dim=1)
+            clf_inputs = self.resize_tensor(clf_inputs, [i // 2 for i in init_shape])
+            out_confidence = self.clf_model(clf_inputs)
+            out_confidence = self.resize_tensor(out_confidence, init_shape)
+            out = (
+                out * out_confidence[:, 0, :, :].unsqueeze(1)
+                + postproc_out * out_confidence[:, 1, :, :].unsqueeze(1)
+            )
         return out
+
+    def resize_tensor(self, tensor, size):
+        import torchvision.transforms.functional as fn
+
+        return fn.resize(tensor, size, antialias=True)
 
     @torch.no_grad()
     def eval_step(self, model, batch, criterion):
