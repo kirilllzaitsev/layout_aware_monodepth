3ee7721539eb51a76eaa7a648d61179a1fc8cd44
kirilllzaitsev <kzaitsev88@gmail.com>
2023-10-15 08:44:12 +0200

    return df embed and df_norm at once

--- layout_aware_monodepth/line_utils.py
+++ layout_aware_monodepth/line_utils.py
@@ -7,15 +7,16 @@ from deeplsd.models.deeplsd import DeepLSD
 
 
 class CustomDeepLSD(DeepLSD):
-    def __init__(self, conf, return_embedding=True):
+    def __init__(self, conf, return_embedding=True, return_both=False):
         # Base network
         super().__init__(conf)
         self.backbone = VGGUNet(tiny=False)
+        self.return_both = return_both
         dim = 64
 
         # Predict the distance field and angle to the nearest line
         # DF head
-        if not return_embedding:
+        if not return_embedding or return_both:
             self.df_head = nn.Sequential(
                 *self.get_common_backbone(dim),
                 nn.Conv2d(64, 1, kernel_size=1),
@@ -28,12 +29,24 @@ class CustomDeepLSD(DeepLSD):
                 nn.Sigmoid(),
             )
         else:
-            self.df_head = nn.Sequential(
+            self.df_extractor = nn.Sequential(
                 *self.get_common_backbone(dim),
             )
-            self.angle_head = nn.Sequential(
+            self.angle_extractor = nn.Sequential(
                 *self.get_common_backbone(dim),
             )
+            self.df_head = self.df_extractor
+            self.angle_head = self.angle_extractor
+
+        self.interim_feature_maps = []
+
+        # Register hooks to capture feature maps
+        idx_df_feature_map_before_final_proj = 3
+        for layer_idx in [idx_df_feature_map_before_final_proj]:
+            self.df_head[layer_idx].register_forward_hook(self.hook_fn)
+
+    def hook_fn(self, module, input, output):
+        self.interim_feature_maps.append(output)
 
     def get_common_backbone(self, dim):
         return (
@@ -46,12 +59,15 @@ class CustomDeepLSD(DeepLSD):
         )
 
     def forward(self, x):
+        self.interim_feature_maps = []
         base = self.backbone(x)
         outputs = {}
         # DF embedding
         outputs["df_norm"] = self.df_head(base).squeeze(1)
         # Closest line direction embedding
         outputs["line_level"] = self.angle_head(base).squeeze(1)
+        if self.return_both:
+            outputs["df_embed"] = self.interim_feature_maps[0]
         return outputs
 
 
@@ -66,12 +82,15 @@ deeplsd_conf = {
 }
 
 
-def load_custom_deeplsd(detect_lines, return_deeplsd_embedding):
+def load_custom_deeplsd(detect_lines, return_deeplsd_embedding, return_both=False):
     conf = deeplsd_conf.copy()
     conf["detect_lines"] = detect_lines
     ckpt = f"{Path(__file__).parent.parent}/artifacts/deeplsd/deeplsd_md.tar"
-    dlsd = CustomDeepLSD(deeplsd_conf, return_embedding=return_deeplsd_embedding)
+    dlsd = CustomDeepLSD(
+        deeplsd_conf, return_embedding=return_deeplsd_embedding, return_both=return_both
+    )
     dlsd.load_state_dict(torch.load(str(ckpt))["model"], strict=False)
+    dlsd = dlsd.eval()
     return dlsd
 
 
